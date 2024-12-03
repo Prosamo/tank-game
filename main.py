43e258d90cb2f1e209cdce84231647318b42740b
Prosamo <s4lm0n.014@gmail.com>
2024-12-03 22:50:00 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -386,12 +386,28 @@ class Result:
             if event.type == pygame.MOUSEBUTTONDOWN:  # 画面を押したとき
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 if 80 <= mouse_y <= 136:
+                    screen.fill((255, 0, 0))
+                    pygame.display.update()
                     url = f'https://x.com/intent/post?text=%E3%82%B9%E3%82%B3%E3%82%A2%E3%81%AF{self.score:.0f}%E3%81%A7%E3%81%97%E3%81%9F%EF%BC%81%0A%E3%83%8F%E3%82%A4%E3%82%B9%E3%82%B3%E3%82%A2%E3%81%AF{self.high_score:.0f}%E3%81%A7%E3%81%97%E3%81%9F%EF%BC%81%0A%0A%E3%81%93%E3%81%A1%E3%82%89%E3%81%8B%E3%82%89%E9%81%8A%E3%81%B9%E3%81%BE%E3%81%99%0Ahttps%3A%2F%2Fprosamo.github.io%2Ftank-game%2F'
+                    #js.window.open(url, '_blank')
+                    #js.window.location.href = url
+                    button_html = f'''
+                                  <div id = tmp_div style="display:none;">
+                                  <a id="tempLink" href="{url}" target="_blank" id="openLinkButton">Open Link</a>
+                                  <script>
+                                  document.getElementById("openLinkButton").addEventListener("click", function(event) {{
+                                      event.preventDefault();
+                                      window.open("{url}", '_blank');
+                                  }});
+                                  </script>
+                                  </div>
+                                  '''
                     script = f"window.open('{url}', '_blank');"
-                    document.body.insertAdjacentHTML('beforeend', f'<a id="tempLink" href="{url}" target="_blank" style="display:none;">Open</a>')
+                    document.body.insertAdjacentHTML('beforeend', button_html)
                     temp_link = document.getElementById('tempLink')
+                    tmp_div = document.getElementById('tmp_div')
                     temp_link.click()
-                    temp_link.remove()
+                    tmp_div.remove()
                 else:
                     game = Game()
                     self.mode = False
